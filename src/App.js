53232d14a27c82160b381f8c33dd0b2784f91766
Gustavo Mendes <gusm217@gmail.com>
2021-07-24 15:55:46 -0300

    Exercise one refatorado para context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './context/contextAPI';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },      
+    };  
+    
+    this.moveCar = this.moveCar.bind(this)
+  }  
+
+  moveCar(car, side) {
+    this.setState({
+      cars: {
+        [car]: side
+      },
+    })
+  }
+
+
+  render(){
+    return(
+    <MyContext.Provider value={{state: this.state.cars, moveCar: this.moveCar}}>   
+      <Cars />   
+    </MyContext.Provider>  
+    )
+  }
 }
 
 export default App;
